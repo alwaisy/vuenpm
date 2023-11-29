a6f25037fc4a9d3891c686c3753ef25fbf9d520c
Awais alwaisy <awaisalwaisy@gmail.com>
2023-11-29 07:30:13 +0500

    chores: add new favicon

--- src/components/shared/VnFooter.vue
+++ src/components/shared/VnFooter.vue
@@ -2,7 +2,11 @@
   <div class="footer">
     <q-separator />
     <div class="q-my-md">
-      <div class="text-h6 q-mb-sm text-weight-bold text-secondary">VueNpm</div>
+      <RouterLink
+        to="/"
+        class="text-h6 q-mb-sm text-weight-bold text-secondary link"
+        >VueNpm</RouterLink
+      >
       <div class="text-subtitle">
         Nothing else, just a simple VueJs packages search engine. Also there is
         a curated list of popular Vue 3 packages.
